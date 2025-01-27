ed4c129049fa3cdad5c923ebd9912b59b129ae54
Staffan Melin <staffan.melin@svenska.gu.se>
2025-01-27 10:16:27 +0100

    Add i18

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import TitleBar from './components/TitleBar.vue'
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
 
+const i18n = createI18n({
+  // something vue-i18n options here ...
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(i18n)
 app.use(router)
 
 app.mount('#app')
