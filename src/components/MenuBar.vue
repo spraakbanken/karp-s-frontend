c855ba012cdd17a7c95ed758bbb47fcf1058d06e
Staffan Melin <staffan.melin@svenska.gu.se>
2025-01-27 08:33:26 +0100

    Add header

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: left;
+}
+
+nav a.router-link-exact-active {
+  color: #f0581a;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+  color: black;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 0.5rem 0 0;
+  color: black;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ src/components/TitleBar.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+import MenuBar from './MenuBar.vue'
+</script>
+
+<template>
+  <div class="header-area">
+    <div class="left-area">
+      <div class="logo-box">
+        <img alt="Karp-S logo" src="@/assets/karps_slogan_sv.svg" width="300" />
+      </div>
+    </div>
+
+    <div class="right-area">
+      <div class="middle-area">
+        <div class="sb-box">
+          <a href="https://spraakbanken.gu.se/">
+            <img src="@/assets/sprakbanken_text_slogan_sv.svg" alt="Språkbanken Text" />
+          </a>
+        </div>
+        <div class="choices-area">
+          <MenuBar />
+        </div>
+      </div>
+      <div class="gu-box">
+        <a href="https://gu.se/">
+          <img src="@/assets/gu-sv.svg" alt="Göteborgs universitet" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .header-area {
+    display: flex;
+    justify-content: space-between;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    padding-right: 2rem;
+    padding-left: 2rem;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: white;
+    box-sizing: border-box;
+    width: 100%;
+  }
+
+  .header-area .left-area {
+    display: flex;
+    box-sizing: border-box;
+  }
+
+  .header-area .logo-box {
+    box-sizing: border-box;
+    margin-right: 1rem;
+  }
+
+  .header-area .right-area {
+    display: flex;
+    box-sizing: border-box;
+  }
+
+  .header-area .middle-area {
+    display: flex;
+    flex-direction: column;
+    align-self: stretch;
+    margin-right: 1rem;
+    box-sizing: border-box;
+  }
+
+  .header-area .sb-box {
+    display: flex;
+    height: 3rem;
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: 0%;
+    justify-content: flex-end;
+    text-align: right;
+    box-sizing: border-box;
+  }
+
+  .sb-box img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+    width: 14rem;
+  }
+
+  .header-area .choices-area {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 0.5rem;
+    gap: 1rem;
+    align-items: baseline;
+  }
+
+  .header-area .gu-box {
+    height: 6rem;
+    margin-top: -1rem;
+    margin-bottom: -1rem;
+    box-sizing: border-box;
+  }
+
+  .gu-box img {
+    height: 100%;
+    vertical-align: middle;
+    box-sizing: border-box;
+  }
+}
+</style>
