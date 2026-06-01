42c76cc13b4ea10d91fc71cd4ceb8144aa625375
Staffan Melin <staffan.melin@svenska.gu.se>
2026-06-01 13:41:55 +0200

    Only import icons that are used

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,53 @@ import { i18n } from '@/i18n.ts'
 
 import App from './App.vue'
 import router from './router'
+// Provide a simple module declaration to satisfy TypeScript when no types are available
+declare module 'vue-matomo'
 import VueMatomo from 'vue-matomo'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fas)
+import { faBackward } from '@fortawesome/free-solid-svg-icons/faBackward'
+import { faBackwardFast } from '@fortawesome/free-solid-svg-icons/faBackwardFast'
+import { faBackwardStep } from '@fortawesome/free-solid-svg-icons/faBackwardStep'
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons/faChevronDown'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons/faChevronLeft'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons/faChevronRight'
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons/faChevronUp'
+import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons/faCircleHalfStroke'
+import { faClose } from '@fortawesome/free-solid-svg-icons/faClose'
+import { faForward } from '@fortawesome/free-solid-svg-icons/faForward'
+import { faForwardFast } from '@fortawesome/free-solid-svg-icons/faForwardFast'
+import { faForwardStep } from '@fortawesome/free-solid-svg-icons/faForwardStep'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons/faGlobe'
+import { faImages } from '@fortawesome/free-solid-svg-icons/faImages'
+import { faLock } from '@fortawesome/free-solid-svg-icons/faLock'
+import { faLockOpen } from '@fortawesome/free-solid-svg-icons/faLockOpen'
+import { faSquare } from '@fortawesome/free-solid-svg-icons/faSquare'
+import { faUserLock } from '@fortawesome/free-solid-svg-icons/faUserLock'
+
+library.add(
+  faBackward,
+  faBackwardFast,
+  faBackwardStep,
+  faCheck,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  faChevronUp,
+  faCircleHalfStroke,
+  faClose,
+  faForward,
+  faForwardFast,
+  faForwardStep,
+  faGlobe,
+  faImages,
+  faLock,
+  faLockOpen,
+  faSquare,
+  faUserLock,
+)
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
